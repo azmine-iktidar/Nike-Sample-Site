111465716d71a6cb72962b83479dbf9b674bf9e8
azmine-iktidar <132399839+azmine-iktidar@users.noreply.github.com>
2023-10-23 07:26:35 +0600

    Popular Products done

--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -1,5 +1,21 @@
+import PopularCards from "../components/PopularCards";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <div className="px-10 font-montserrat space-y-8">
+      <h1 className="text-4xl font-bold">
+        Our Popular <span className="text-coral-red">Products</span>
+      </h1>
+      <p className="w-1/2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam delectus
+        a neque perspiciatis voluptate distinctio possimus, soluta deleniti eius
+        labore eum.
+      </p>
+      <section>
+        <PopularCards />
+      </section>
+    </div>
+  );
 };
 
 export default PopularProducts;
