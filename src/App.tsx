5ff3b91d0c2a3f84f537c35e833dd7ba7a6f22b2
azmine-iktidar <132399839+azmine-iktidar@users.noreply.github.com>
2023-10-23 06:36:21 +0600

    Hero Section Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import CustomerReviews from "./sections/CustomerReviews";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import PopularProducts from "./sections/PopularProducts";
 import Services from "./sections/Services";
@@ -7,36 +9,52 @@ import SplOffer from "./sections/SplOffer";
 import Subscribe from "./sections/Subscribe";
 import SuperQuality from "./sections/SuperQuality";
 
-const App = () => {
+import { useRef } from "react";
+
+function App() {
+  const ref = useRef(null);
+
+  const options = {
+    smooth: true,
+  };
+
   return (
-    <main className="relative">
-      App
-      <Navbar />
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section>
-        {/* dgfgfgdfghjk */}
-        <PopularProducts />
-      </section>
-      <section>
-        <SuperQuality />
-      </section>
-      <section className="padding-x py-10">
-        <Services />
-      </section>
-      <section className="padding">
-        <SplOffer />
-      </section>
-      <section className="bg-pale-blue padding">
-        <CustomerReviews />
-      </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe />
-      </section>
-      <section className="padding-x bg-black padding-t pb-8 text-white">
-        <Footer />
-      </section>
-    </main>
+    <LocomotiveScrollProvider options={options} containerRef={ref}>
+      <main
+        data-scroll-container
+        ref={ref}
+        className="relative"
+        data-scroll-speed="4"
+      >
+        {/* <Navbar /> */}
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          <Hero />
+        </section>
+
+        <section>
+          <PopularProducts />
+        </section>
+        <section>
+          <SuperQuality />
+        </section>
+        <section className="padding-x py-10">
+          <Services />
+        </section>
+        <section className="padding">
+          <SplOffer />
+        </section>
+        <section className="bg-pale-blue padding">
+          <CustomerReviews />
+        </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+          <Subscribe />
+        </section>
+        <section className="padding-x bg-black padding-t pb-8 text-white">
+          <Footer />
+        </section>
+      </main>
+    </LocomotiveScrollProvider>
   );
-};
+}
 
 export default App;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
+import Shoecard from "../components/Shoecard";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [shoeUrl, setShoeUrl] = useState<string>(bigShoe1);
+  return (
+    <div className="w-full flex xl:flex-row min-h-screen max-container">
+      <section className="w-2/4 font-montserrat space-y-8 pl-10 max-lg:space-y-2 items-start justify-center flex flex-col">
+        <p className="text-xs text-coral-red">Our Summer Collection</p>
+        <h1 className="font-bold text-4xl max-lg:text-2xl bg-white flex-nowrap break-keep">
+          The New Definition
+        </h1>
+        <h1 className="font-bold text-4xl max-lg:text-2xl">
+          of
+          <span className="text-coral-red inline-block ">{" Nike"}</span>
+        </h1>
+        <p className="text-xs">
+          Discover the new nike shoes for your daily comfort and casual events.
+          <br />
+          Be confident and comfortable with the new Nike shoes.
+        </p>
+        <button className="border rounded-full px-3 py-1 bg-coral-red flex justify-center items-center gap-2 max-lg:text-[12px]">
+          Shop Now <img src={arrowRight} />
+        </button>
+        <section id="numbers" className="flex gap-5 pt-5">
+          <div>
+            <p className="text-2xl font-bold">2k+</p>
+            Brands
+          </div>
+          <div>
+            <p className="text-2xl font-bold">500+</p>
+            Shops
+          </div>
+          <div>
+            <p className="text-2xl font-bold">260k+</p>
+            Customers
+          </div>
+        </section>
+      </section>
+      <section className="bg-red-200 w-1/2 flex items-center justify-center bg-cover bg-center bg-hero flex-col relative">
+        <img
+          src={shoeUrl}
+          width={400}
+          height={500}
+          className="object-contain relative z-10"
+        />
+        <div className="flex absolute bottom-[-30px] space-x-5">
+          <Shoecard shoeUrl={shoeUrl} imgChange={setShoeUrl} />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Hero;
